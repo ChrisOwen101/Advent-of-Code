786269c9913d90ac48b4f6d17e04e0fda37d2c18
Chris Owen <chris@sigmalabs.xyz>
2022-12-14 14:20:11 +0000

    Day 8 Part 2

--- Day 8/trees.js
+++ Day 8/trees.js
@@ -9,12 +9,18 @@ function readElfGatheringData(filename, callback) {
   });
 }
 
-function calculateTrees(inputForest) {
+function generateForest(inputForest) {
   const forest = inputForest.split("\n").map((row) => {
     return row.split("").map((treeHeight) => {
       return new Tree(parseInt(treeHeight));
     });
   });
+
+  return forest;
+}
+
+function calculateTrees(inputForest) {
+  const forest = generateForest(inputForest);
 
   let visibleCount = 0;
 
@@ -116,23 +122,115 @@ function checkVisible(x, y, forest) {
 
 class Tree {
   visible = false;
+  scenic = -1;
 
   constructor(height) {
     this.height = height;
   }
 }
 
-//Should print 9
+function calculateScenicScore(forest, x, y) {
+  //Check Up
+  let upCount = 0;
+
+  for (let i = x; i >= 0; i--) {
+    if (i === x && y === y) {
+      continue;
+    }
+
+    const inspectTree = forest[i][y];
+    const currentTree = forest[x][y];
+    upCount++;
+
+    if (inspectTree.height >= currentTree.height) {
+      break;
+    }
+  }
+
+  let downCount = 0;
+
+  for (let i = x; i < forest.length; i++) {
+    if (i === x && y === y) {
+      continue;
+    }
+
+    const inspectTree = forest[i][y];
+    const currentTree = forest[x][y];
+    downCount++;
+
+    if (inspectTree.height >= currentTree.height) {
+      break;
+    }
+  }
+
+  let rightCount = 0;
+  for (let i = y; i < forest[x].length; i++) {
+    if (x === x && i === y) {
+      continue;
+    }
+
+    const inspectTree = forest[x][i];
+    const currentTree = forest[x][y];
+    rightCount++;
+
+    if (inspectTree.height >= currentTree.height) {
+      break;
+    }
+  }
+
+  let leftCount = 0;
+  for (let i = y; i >= 0; i--) {
+    if (x === x && i === y) {
+      continue;
+    }
+
+    const inspectTree = forest[x][i];
+    const currentTree = forest[x][y];
+    leftCount++;
+
+    if (inspectTree.height >= currentTree.height) {
+      break;
+    }
+  }
+
+  return upCount * downCount * leftCount * rightCount;
+}
+
+function calculateMaxScenicScore(inputForest) {
+  const forest = generateForest(inputForest);
+
+  let max = 0;
+
+  forest.forEach((forestRow, x) => {
+    forestRow.forEach((tree, y) => {
+      const score = calculateScenicScore(forest, x, y);
+      tree.scenic = score;
+
+      if (tree.scenic > max) {
+        max = tree.scenic;
+      }
+    });
+  });
+
+  console.log(forest);
+
+  return max;
+}
+
+//Should print 1
 readElfGatheringData("simple.txt", (data) => {
   //console.log(calculateTrees(data));
+  //console.log(calculateMaxScenicScore(data));
 });
 
-//Should print 21
+//Should print 8
 readElfGatheringData("test.txt", (data) => {
   //console.log(calculateTrees(data));
+  //console.log(calculateMaxScenicScore(data));
 });
 
 //Be our solution
 readElfGatheringData("trees.txt", (data) => {
-  console.log(calculateTrees(data));
+  //console.log(calculateTrees(data));
+  console.log(calculateMaxScenicScore(data));
 });
